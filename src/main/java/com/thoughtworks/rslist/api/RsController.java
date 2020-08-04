f46639ce0358b8a82231c9d10e2426d32d87ffcc
maqing <mr.maqing@gmail.com>
2020-08-04 17:25:39 +0800

    [maqing]can get one rs

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private List<String> rsList = new ArrayList<>(Arrays.asList("第一条事件", "第二条事件", "第三条事件"));
+
+    @GetMapping("/rs/{index}")
+    String getOneRs(@PathVariable int index) {
+        return rsList.get(index - 1);
+    }
 }
