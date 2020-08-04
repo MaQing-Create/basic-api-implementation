b4f6db88fae2f8916fcc998799dcc11974f6108f
maqing <mr.maqing@gmail.com>
2020-08-04 17:33:24 +0800

    [maqing] can get rs bewteen

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +16,13 @@ public class RsController {
     @GetMapping("/rs/{index}")
     String getOneRs(@PathVariable int index) {
         return rsList.get(index - 1);
+    }
+
+    @GetMapping("/rs/list")
+    String getRsBetween(@RequestParam(required = false) Integer start, @RequestParam(required = false) Integer end) {
+        if (start == null && end == null) return rsList.toString();
+        if (start == null) return rsList.subList(0, end).toString();
+        if (end == null) return rsList.subList(start - 1, rsList.size()).toString();
+        return rsList.subList(start - 1, end).toString();
     }
 }
